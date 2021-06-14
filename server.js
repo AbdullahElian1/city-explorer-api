225ec46f803eac7326a8a309211964308185c52e
AbdullahElian1 <abodelian28@gmail.com>
2021-06-14 17:55:25 +0300

    sloving lab 07

---
+++ server.js
@@ -0,0 +1,59 @@
+const express=require('express');
+const server=express();
+const weatherData = require('./data/weather.json');
+
+const PORT = 3010; 
+
+const arr=[];
+
+//locallhost:3010/test
+server.get('/test',(req,res)=>{
+    res.send("hello from test route ");
+    
+});
+
+//localhost:3010/getNames?cityLan=-33.87&cityLon=151.21
+server.get('/getNames',(req,res)=>{
+let lan=req.query.cityLan;
+let lon=req.query.cityLon;
+    let cityNames = weatherData.city.find(item=>{
+        if(item.lat == lan && item.lon==lon)
+        return item.city_name;
+    })
+    let forecast1 =  new Forecast(cityNames.date,cityNames.description) ;
+    arr.push(forecast1);
+    res.send({cityNames,arr});
+})
+
+class Forecast {
+    
+    constructor( date1, description1){
+         this.date = date1;
+         this.description= description1;
+        
+
+    }
+
+
+    
+}
+
+
+
+
+
+//locallhost:3010/getName?cityName=Amman
+
+    
+    //localhost:3010 .....
+server.get('*',(req,res) =>{
+    res.status(404).send('sorry, this page not found');
+})
+
+
+
+
+server.listen(PORT,()=>{
+    console.log(`im here ${PORT}`);
+});
+
